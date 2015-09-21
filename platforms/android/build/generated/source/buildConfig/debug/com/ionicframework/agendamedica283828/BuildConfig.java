daa249ba87f5382ddda998a58931d0895f0b443e
Eduardo Souza <eduardo.souza@pipeline.com.br>
2015-09-21 16:25:24 -0300

    adicionando o layout basico do ionic

---
+++ platforms/android/build/generated/source/buildConfig/debug/com/ionicframework/agendamedica283828/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.ionicframework.agendamedica283828;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.ionicframework.agendamedica283828";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 18;
+  public static final String VERSION_NAME = "";
+}
